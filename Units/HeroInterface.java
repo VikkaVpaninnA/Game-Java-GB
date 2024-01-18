521b6e1a338801cd6925aa96bb26ad705f661001
vik <vikki>
2024-01-18 14:42:38 +0300

    commit Units

---
+++ Units/HeroInterface.java
@@ -0,0 +1,16 @@
+package OOP.Units;
+
+public interface HeroInterface {
+    public static int getHealth() { return 0; }
+    public static void setHealth( int health ) { }
+    public static void setState( int state ) { }
+
+    // Получаем урон
+    public static void doDamage( int damage ) {
+        setHealth( getHealth() - damage );
+        if ( getHealth() <= 0 ) {
+            setHealth( 0 );
+            setState ( -1 );
+        }
+    }
+}
